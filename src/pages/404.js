46febf3e24830339aa15f269322a54b4bfab2d81
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-20 02:25:10 -0700

    added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,15 @@
+import { Box, Center, Heading } from '@chakra-ui/layout';
+import React from 'react';
+
+export default function Bruh() {
+  return (
+    <Box p={6}>
+      <Center>
+        <Heading
+          fontSize="xxx-large">
+          BRUH... This page doesn't exist
+        </Heading>
+      </Center>
+    </Box>
+  )
+}
