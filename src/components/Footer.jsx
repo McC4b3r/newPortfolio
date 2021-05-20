622a6e088ee7d85a8433849d1936dfc5ffcbacd1
Ryan McCabe <ryan.mccabe089@gmail.com>
2021-05-20 02:29:38 -0700

    adjusted links so that a new tab is opened

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Center, HStack, Icon, Spacer } from '@chakra-ui/react';
+import { Box, Center, HStack, Icon } from '@chakra-ui/react';
 import React from 'react';
 import { GoMarkGithub } from "@react-icons/all-files/go/GoMarkGithub";
 import { ImLinkedin } from "@react-icons/all-files/im/ImLinkedin";
@@ -8,13 +8,19 @@ const Footer = () => {
     <Box>
       <Center h="100px">
         <HStack spacing="20px">
-          <a href="https://github.com/mcc4b3r" >
+          <a
+            href="https://github.com/mcc4b3r"
+            target="_blank"
+            rel="noopener noreferrer" >
             <Icon
               as={GoMarkGithub}
               w={8}
               h={8} />
           </a>
-          <a href="https://www.linkedin.com/in/ryan-mccabe-089/">
+          <a
+            href="https://www.linkedin.com/in/ryan-mccabe-089/"
+            target="_blank"
+            rel="noopener noreferrer">
             <Icon
               as={ImLinkedin}
               w={8}
